b94d7f63388ce3dfae737a33171264658df22ce8
Exapos <knobisak@seznam.cz>
2023-03-02 22:06:42 +0100

    WIP

--- projekt-2023-hlavek/App.js
+++ projekt-2023-hlavek/App.js
@@ -1,10 +1,31 @@
+/**
+*  https://www.figma.com/file/md1gzwMT9bVJPeOToZ5GoO/Untitled?node-id=0%3A1&t=FaapmYF0gwFvHkGA-1
+*
+*
+*TODO:
+*  - Naučit to Péťu
+*
+*  - Minimal Requirements 
+*
+*  - App is running on real mobile device (not on virtual simulator) 
+*
+*  - App has minimal three View and user can change between them 
+*
+*  - App has simple form with few imputs (f.g. input, switch, radio button, dropdown menu) 
+*
+*  - App has button to save inputs into local database storage 
+*
+*  - App on one view you shoul present records from local database 
+*/
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import SignInScreen from './src/screens/SignInScreen/SignInScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello this is basic test of our application!</Text>
+      <SignInScreen />
       <StatusBar style="auto" />
     </View>
   );
